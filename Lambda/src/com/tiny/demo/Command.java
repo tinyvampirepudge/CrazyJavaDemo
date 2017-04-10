5a8573fd3a22ceea9b873ecfc0cfc361500b777d
tinytongtong <879595361@qq.com>
2017-04-10 10:41:47 +0800

    Arrays工具类

---
+++ Lambda/src/com/tiny/demo/Command.java
@@ -0,0 +1,6 @@
+package com.tiny.demo;
+
+public interface Command {
+	//接口里面定义的process方法用于风中“处理行为”
+	void process(int[] target);
+}
---
+++ Lambda/src/com/tiny/demo/ProcessArray.java
@@ -0,0 +1,7 @@
+package com.tiny.demo;
+
+public class ProcessArray {
+	public void process(int[] target,Command cmd){
+		cmd.process(target);
+	}
+}
